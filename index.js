71beea4b0dd7fd853aecc80417377cc57ac7f854
Kieu <kieuphan530@gmail.com>
2021-09-09 01:43:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Kathy is the champion";
+
+document.newHeader.apprend('h1');
+
